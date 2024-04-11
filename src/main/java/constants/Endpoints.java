509ef323b0b0f68e0332460e4576a79ef0d9c2e7
Artem Vavilchenkov <vavilzidane007@mail.ru>
2024-04-12 00:45:23 +0400

    add constants

---
+++ src/main/java/constants/Endpoints.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Endpoints {
+
+    public static final String REGISTER = "/api/auth/register/";//POST
+    public static final String LOGIN = "/api/auth/login/";//POST
+    public  static final String USER = "/api/auth/user/";//GET //DELETE //Patch
+}
